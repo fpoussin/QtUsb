4f839bea2a56f4060b25c2924611f04882995ba4
Fabien Poussin <fabien.poussin@gmail.com>
2015-07-20 15:00:56 +0200

    package script fixes

--- package/make_deb.py
+++ package/make_deb.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python
 
 import sys, os, re, argparse, iso8601, shutil
-from subprocess import call, check_output, Popen, CalledProcessError
+from subprocess import call, check_output, Popen, CalledProcessError, STDOUT, PIPE
 from xml.etree import ElementTree as ET
+from traceback import print_exc
 
 releases = ["trusty", "vivid", "utopic"]
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-r', '--release', help='The Ubuntu release')
 parser.add_argument('-s', '--source', help='Build signed source package', action="store_true")
-parser.add_argument('-b', '--bin', help='Build local binary package', action="store_true")
+parser.add_argument('-lb', '--bin', help='Build local binary package', action="store_true")
 parser.add_argument('-p', '--ppa', help='Send source package to PPA', action="store_true")
+parser.add_argument('-b', '--sbuild', help='Build binary package with sbuild', action="store_true")
 
+
+def run_cmd(cmd, do_print=True, **kwargs):
+    p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, **kwargs)
+    if do_print:
+        lines_iterator = iter(p.stdout.readline, b"")
+        for line in lines_iterator:
+            print(line.strip())  # yield line
+    else:
+        p.wait()
 
 def makeChangelog(release, dest, rev):
     # ~ f = open(dest+"/debian/changelog", "r")
@@ -29,11 +40,11 @@ def copySrc(dest, rev):
     cmd = "find .. -maxdepth 1 \! \( -name package -o -name *.pro.user* -o -name .. \) -exec cp -vr '{}' " + dest + "/ ';'"
     print check_output([cmd], shell=True)
 
-    check_output(["cd " + dest + "; echo "" | dh_make -n --single -e fabien.poussin@gmail.com -c gpl3"],
+    check_output(["cd {0}; echo '' | dh_make -n --single -e fabien.poussin@gmail.com -c gpl3".format(dest)],
                        shell=True)
 
-    print check_output(["cp -v debian/* " + dest + "/debian/"], shell=True)
-    check_output(["rm -f " + dest + "/debian/*.ex " + dest + "/debian/*.EX " + dest + "/debian/ex.*"], shell=True)
+    print check_output(["cp -v debian/* {0}/debian/".format(dest)], shell=True)
+    check_output(["rm -f {0}/debian/*.ex {0}/debian/*.EX {0}/debian/ex.*".format(dest)], shell=True)
 
     f = open(dest + "/debian/changelog", "r")
     tmp = f.read().replace("unstable; urgency=low", args.release + "; urgency=medium")
@@ -49,16 +60,18 @@ def copySrc(dest, rev):
 
 def makeSrc(dest):
     print "Building Source package"
-    print check_output(["cd " + dest + "; debuild -j4 -S -sa"], shell=True)
+    run_cmd(["cd {0}; debuild -j4 -S -sa".format(dest)])
 
+def makeSBuild(dest):
+    print "Building binary package (sbuild)"
+    run_cmd(["sbuild -vd {0} {1}".format(args.release, dest)])
 
 def makeBin(dest):
     print "Building binary package"
-    print check_output(["cd " + dest + "; debuild -j4 -b -uc -us"], shell=True)
-
+    run_cmd(["cd {0}; debuild -j4 -b -uc -us".format(dest)])
 
 def sendSrc(ver):
-    print check_output(["dput ppa:fpoussin/ppa " + ver + "_source.changes"], shell=True)
+    run_cmd(["dput ppa:fpoussin/ppa {0}_source.changes".format(ver)])
 
 
 if __name__ == "__main__":
@@ -70,30 +83,37 @@ if __name__ == "__main__":
 
     if args.release not in releases:
         print "Invalid Ubuntu release, please chose among:", releases
-        sys.exit(1)
+        exit(1)
 
     ver = check_output(["grep \"VERSION =\" ../QtUsb.pro | awk '{ print $3 }' "], shell=True).replace('\n', '')
     if not ver:
         print "Could not fetch last version"
-        sys.exit(1)
+        exit(1)
 
     print "Last version:", ver
 
-folder_name = 'libqt5usb5-{0}~{1}'.format(ver, args.release)
+    folder_name = 'libqt5usb5-{0}~{1}'.format(ver, args.release)
 
-print "Package version:", folder_name
+    print "Package version:", folder_name
 
-copySrc(folder_name, ver)
-makeChangelog(args.release, folder_name, ver)
+    copySrc(folder_name, ver)
+    makeChangelog(args.release, folder_name, ver)
 
-try:
-    if args.source:
-        makeSrc(folder_name)
-        if args.ppa:
-            sendSrc('libqt5usb5_' + str(ver) + '~' + args.release)
-    if args.bin:
-        makeBin(folder_name)
+    try:
+        if args.source:
+            makeSrc(folder_name)
+            if args.ppa:
+                sendSrc(folder_name)
+        if args.sbuild:
+            if not args.source:  # Need to make sources first
+                makeSrc(folder_name)
+            makeSBuild(folder_name)
+        if args.bin:
+            makeBin(folder_name)
 
-    print check_output(["rm -rf " + folder_name], shell=True)
-except CalledProcessError as e:
-    print e.output
+        print check_output(["rm -rf " + folder_name], shell=True)
+    except CalledProcessError as e:
+        print e.output
+    except OSError as e:
+        print e.filename, e
+        print print_exc()
