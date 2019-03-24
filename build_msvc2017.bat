be4d6d096e65e05a28ff91671a9920fd6a7e85a6
Fabien Poussin <fabien.poussin@gmail.com>
2019-03-25 00:34:25 +0100

    Fixed batch script for x86

--- build_msvc2017.bat
+++ build_msvc2017.bat
@@ -3,12 +3,9 @@ SET ARCH=%1
 SET QTDIR=%2
 
 SET vcarch=%ARCH%
+SET usbarch=%ARCH%
 IF "%ARCH%" == "x64" SET vcarch=amd64
-IF "%ARCH%" == "Win32" SET vcarch=x86
-
-SET usbarch=%vcarch%
-IF "%vcarch%" == "x64" SET usbarch=x64
-IF "%vcarch%" == "x86" SET usbarch=Win32
+IF "%ARCH%" == "x86" SET usbarch=Win32
 
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" %vcarch%
 
@@ -18,8 +15,8 @@ git submodule update --init --recursive
 CD libusb
 msbuild msvc\libusb_static_2017.vcxproj /p:Configuration=Release
 msbuild msvc\libusb_static_2017.vcxproj /p:Configuration=Debug
-COPY %ARCH%\Release\lib\libusb-1.0.lib ..\libusb-1.0.lib
-COPY %ARCH%\Debug\lib\libusb-1.0.lib ..\libusb-1.0d.lib
+COPY %usbarch%\Release\lib\libusb-1.0.lib ..\libusb-1.0.lib
+COPY %usbarch%\Debug\lib\libusb-1.0.lib ..\libusb-1.0d.lib
 CD ..
 
 RMDIR /S /Q build
