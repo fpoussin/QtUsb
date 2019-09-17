545663dc89e2f987a7aabde2bb631438ed27c28b
Unknown <andre.lintelmann@hertz.st>
2019-09-17 14:45:15 +0200

    fix indention

--- src/usb/qusbdevice.cpp
+++ src/usb/qusbdevice.cpp
@@ -11,7 +11,7 @@ QUsbDevicePrivate::QUsbDevicePrivate()
 {
     int r = libusb_init(&m_ctx);
     if (r < 0) {
-	qCritical("LibUsb Init Error %d", r);
+        qCritical("LibUsb Init Error %d", r);
     }
     m_devHandle = Q_NULLPTR;
 
@@ -176,18 +176,18 @@ QUsbDevice::~QUsbDevice()
 QByteArray QUsbDevice::speedString() const
 {
     switch (m_spd) {
-    case unknownSpeed:
-	return "Unknown speed";
-    case lowSpeed:
-	return "Low speed";
-    case fullSpeed:
-	return "Full speed";
-    case highSpeed:
-	return "High speed";
-    case superSpeed:
-	return "Super speed";
-    case superSpeedPlus:
-	return "Super speed plus";
+        case unknownSpeed:
+            return "Unknown speed";
+        case lowSpeed:
+            return "Low speed";
+        case fullSpeed:
+            return "Full speed";
+        case highSpeed:
+            return "High speed";
+        case superSpeed:
+            return "Super speed";
+        case superSpeedPlus:
+            return "Super speed plus";
     }
 
     return "Error";
@@ -201,34 +201,34 @@ QUsbDevice::DeviceStatus QUsbDevice::status() const
 QByteArray QUsbDevice::statusString() const
 {
     switch (m_status) {
-    case statusOK:
-	return "Success (no error)";
-    case statusIoError:
-	return "Input/output error";
-    case statusInvalidParam:
-	return "Invalid parameter";
-    case statusAccessDenied:
-	return "Access denied (insufficient permissions)";
-    case statusNoSuchDevice:
-	return "No such device (it may have been disconnected)";
-    case statusNotFound:
-	return "Entity not found";
-    case statusBusy:
-	return "Resource busy";
-    case statusTimeout:
-	return "Operation timed out";
-    case statusOverflow:
-	return "Overflow";
-    case statusPipeError:
-	return "Pipe error";
-    case statusInterrupted:
-	return "System call interrupted (perhaps due to signal)";
-    case statusNoMemory:
-	return "Insufficient memory";
-    case statusNotSupported:
-	return "Operation not supported or unimplemented on this platform";
-    case statusUnknownError:
-	break;
+        case statusOK:
+            return "Success (no error)";
+        case statusIoError:
+            return "Input/output error";
+        case statusInvalidParam:
+            return "Invalid parameter";
+        case statusAccessDenied:
+            return "Access denied (insufficient permissions)";
+        case statusNoSuchDevice:
+            return "No such device (it may have been disconnected)";
+        case statusNotFound:
+            return "Entity not found";
+        case statusBusy:
+            return "Resource busy";
+        case statusTimeout:
+            return "Operation timed out";
+        case statusOverflow:
+            return "Overflow";
+        case statusPipeError:
+            return "Pipe error";
+        case statusInterrupted:
+            return "System call interrupted (perhaps due to signal)";
+        case statusNoMemory:
+            return "Insufficient memory";
+        case statusNotSupported:
+            return "Operation not supported or unimplemented on this platform";
+        case statusUnknownError:
+            break;
     }
 
     return "Other error";
@@ -239,9 +239,9 @@ void QUsbDevice::handleUsbError(int error_code)
     DbgPrintFuncName();
     DeviceStatus status = static_cast<QUsbDevice::DeviceStatus>(error_code);
     if (status != m_status) {
-	m_status = status;
-	qWarning("Usb device status changed: %s", statusString().constData());
-	emit statusChanged(m_status);
+        m_status = status;
+        qWarning("Usb device status changed: %s", statusString().constData());
+        emit statusChanged(m_status);
     }
 }
 
@@ -259,22 +259,22 @@ QUsbDevice::IdList QUsbDevice::devices()
     libusb_set_debug(ctx, LIBUSB_LOG_LEVEL_NONE);
     cnt = libusb_get_device_list(ctx, &devs); // get the list of devices
     if (cnt < 0) {
-	qCritical("libusb_get_device_list Error");
-	libusb_free_device_list(devs, 1);
-	return list;
+        qCritical("libusb_get_device_list Error");
+        libusb_free_device_list(devs, 1);
+        return list;
     }
 
     for (int i = 0; i < cnt; i++) {
-	libusb_device *dev = devs[i];
-	libusb_device_descriptor desc;
+        libusb_device *dev = devs[i];
+        libusb_device_descriptor desc;
 
-	if (libusb_get_device_descriptor(dev, &desc) == 0) {
-	    Id filter;
-	    filter.pid = desc.idProduct;
-	    filter.vid = desc.idVendor;
+        if (libusb_get_device_descriptor(dev, &desc) == 0) {
+            Id filter;
+            filter.pid = desc.idProduct;
+            filter.vid = desc.idVendor;
 
-	    list.append(filter);
-	}
+            list.append(filter);
+        }
     }
 
     libusb_free_device_list(devs, 1);
@@ -295,88 +295,88 @@ qint32 QUsbDevice::open()
     libusb_device *dev = Q_NULLPTR;
 
     if (m_connected)
-	return -1;
+        return -1;
 
     cnt = libusb_get_device_list(d->m_ctx, &d->m_devs); // get the list of devices
     if (cnt < 0) {
-	qCritical("libusb_get_device_list error");
-	libusb_free_device_list(d->m_devs, 1);
-	return -1;
+        qCritical("libusb_get_device_list error");
+        libusb_free_device_list(d->m_devs, 1);
+        return -1;
     }
 
     for (int i = 0; i < cnt; i++) {
-	dev = d->m_devs[i];
-	libusb_device_descriptor desc;
+        dev = d->m_devs[i];
+        libusb_device_descriptor desc;
 
-	if (libusb_get_device_descriptor(dev, &desc) == 0) {
-	    if (desc.idProduct == m_id.pid && desc.idVendor == m_id.vid) {
-		if (m_log_level >= logInfo)
-		    qInfo("Found device");
+        if (libusb_get_device_descriptor(dev, &desc) == 0) {
+            if (desc.idProduct == m_id.pid && desc.idVendor == m_id.vid) {
+                if (m_log_level >= logInfo)
+                    qInfo("Found device");
 
-		rc = libusb_open(dev, &d->m_devHandle);
-		if (rc == 0)
-		    break;
-		else if (m_log_level >= logWarning) {
-		    qWarning("Failed to open device: %s", libusb_strerror(static_cast<enum libusb_error>(rc)));
-		}
-	    }
-	}
+                rc = libusb_open(dev, &d->m_devHandle);
+                if (rc == 0)
+                    break;
+                else if (m_log_level >= logWarning) {
+                    qWarning("Failed to open device: %s", libusb_strerror(static_cast<enum libusb_error>(rc)));
+                }
+            }
+        }
     }
     libusb_free_device_list(d->m_devs, 1); // free the list, unref the devices in it
 
     if (rc != 0 || d->m_devHandle == Q_NULLPTR) {
-	return rc;
+        return rc;
     }
 
     if (m_log_level >= logInfo)
-	qInfo("Device Open");
+        qInfo("Device Open");
 
     if (libusb_kernel_driver_active(d->m_devHandle, m_config.interface) == 1) { // find out if kernel driver is attached
-	if (m_log_level >= logDebug)
-	    qDebug("Kernel Driver Active");
-	if (libusb_detach_kernel_driver(d->m_devHandle, m_config.interface) == 0) // detach it
-	    if (m_log_level >= logDebug)
-		qDebug("Kernel Driver Detached!");
+        if (m_log_level >= logDebug)
+            qDebug("Kernel Driver Active");
+        if (libusb_detach_kernel_driver(d->m_devHandle, m_config.interface) == 0) // detach it
+            if (m_log_level >= logDebug)
+                qDebug("Kernel Driver Detached!");
     }
 
     int conf;
     libusb_get_configuration(d->m_devHandle, &conf);
 
     if (conf != m_config.config) {
-	if (m_log_level >= logInfo)
-	    qInfo("Configuration needs to be changed");
-	rc = libusb_set_configuration(d->m_devHandle, m_config.config);
-	if (rc != 0) {
-	    if (m_log_level >= logWarning)
-		qWarning("Cannot Set Configuration");
-	    handleUsbError(rc);
-	    return -3;
-	}
+        if (m_log_level >= logInfo)
+            qInfo("Configuration needs to be changed");
+        rc = libusb_set_configuration(d->m_devHandle, m_config.config);
+        if (rc != 0) {
+            if (m_log_level >= logWarning)
+                qWarning("Cannot Set Configuration");
+            handleUsbError(rc);
+            return -3;
+        }
     }
     rc = libusb_claim_interface(d->m_devHandle, m_config.interface);
     if (rc != 0) {
-	if (m_log_level >= logWarning)
-	    qWarning("Cannot Claim Interface");
-	handleUsbError(rc);
-	return -4;
+        if (m_log_level >= logWarning)
+            qWarning("Cannot Claim Interface");
+        handleUsbError(rc);
+        return -4;
     }
 
     switch (libusb_get_device_speed(dev)) {
-    case LIBUSB_SPEED_LOW:
-	this->m_spd = lowSpeed;
-	break;
-    case LIBUSB_SPEED_FULL:
-	this->m_spd = fullSpeed;
-	break;
-    case LIBUSB_SPEED_HIGH:
-	this->m_spd = highSpeed;
-	break;
-    case LIBUSB_SPEED_SUPER:
-	this->m_spd = superSpeed;
-	break;
-    default:
-	this->m_spd = unknownSpeed;
-	break;
+        case LIBUSB_SPEED_LOW:
+            this->m_spd = lowSpeed;
+            break;
+        case LIBUSB_SPEED_FULL:
+            this->m_spd = fullSpeed;
+            break;
+        case LIBUSB_SPEED_HIGH:
+            this->m_spd = highSpeed;
+            break;
+        case LIBUSB_SPEED_SUPER:
+            this->m_spd = superSpeed;
+            break;
+        default:
+            this->m_spd = unknownSpeed;
+            break;
     }
 
     m_connected = true;
@@ -394,13 +394,13 @@ void QUsbDevice::close()
     Q_D(QUsbDevice);
 
     if (d->m_devHandle && m_connected) {
-	// stop any further write attempts whilst we close down
-	if (m_log_level >= logInfo)
-	    qInfo("Closing USB connection");
+        // stop any further write attempts whilst we close down
+        if (m_log_level >= logInfo)
+            qInfo("Closing USB connection");
 
-	libusb_release_interface(d->m_devHandle, 0); // release the claimed interface
-	libusb_close(d->m_devHandle); // close the device we opened
-	d->m_devHandle = Q_NULLPTR;
+        libusb_release_interface(d->m_devHandle, 0); // release the claimed interface
+        libusb_close(d->m_devHandle); // close the device we opened
+        d->m_devHandle = Q_NULLPTR;
     }
 
     m_connected = false;
@@ -415,9 +415,9 @@ void QUsbDevice::setLogLevel(LogLevel level)
     Q_D(QUsbDevice);
     m_log_level = level;
     if (level >= logDebugAll)
-	libusb_set_debug(d->m_ctx, LIBUSB_LOG_LEVEL_DEBUG);
+        libusb_set_debug(d->m_ctx, LIBUSB_LOG_LEVEL_DEBUG);
     else
-	libusb_set_debug(d->m_ctx, LIBUSB_LOG_LEVEL_NONE);
+        libusb_set_debug(d->m_ctx, LIBUSB_LOG_LEVEL_NONE);
 }
 
 /*!
@@ -513,13 +513,13 @@ void QUsbEventsThread::run()
 
     timeval t = { 0, 100000 };
     while (!this->isInterruptionRequested()) {
-	if (libusb_event_handling_ok(m_ctx) == 0) {
-	    libusb_unlock_events(m_ctx);
-	    break;
-	}
-	if (libusb_handle_events_timeout_completed(m_ctx, &t, Q_NULLPTR) != 0) {
-	    break;
-	}
+        if (libusb_event_handling_ok(m_ctx) == 0) {
+            libusb_unlock_events(m_ctx);
+            break;
+        }
+        if (libusb_handle_events_timeout_completed(m_ctx, &t, Q_NULLPTR) != 0) {
+            break;
+        }
     }
 }
 
@@ -531,8 +531,8 @@ void QUsbEventsThread::run()
 bool QUsbDevice::Config::operator==(const QUsbDevice::Config &other) const
 {
     return other.config == config &&
-	   other.interface == interface &&
-	   other.alternate == alternate;
+            other.interface == interface &&
+            other.alternate == alternate;
 }
 
 QUsbDevice::Config &QUsbDevice::Config::operator=(const QUsbDevice::Config &other)
@@ -551,7 +551,7 @@ QUsbDevice::Config &QUsbDevice::Config::operator=(const QUsbDevice::Config &othe
 bool QUsbDevice::Id::operator==(const QUsbDevice::Id &other) const
 {
     return other.pid == pid &&
-	   other.vid == vid;
+            other.vid == vid;
 }
 
 QUsbDevice::Id &QUsbDevice::Id::operator=(const QUsbDevice::Id &other)
