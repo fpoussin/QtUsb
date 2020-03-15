dcd481fcd36188df5a1f36b7b6e8769b284a42d0
Fabien Poussin <fabien.poussin@gmail.com>
2020-03-16 00:13:43 +0100

    Fixed msvc build script

--- build_msvc.bat
+++ build_msvc.bat
@@ -2,14 +2,36 @@
 setlocal
 
 IF "%1"=="" (
+  echo "Missing MSVC version [2017|2019]"
+  pause
+  exit /b 1c
+)
+
+IF "%2"=="" (
+  echo "Missing architecture [x86|x64]"
+  pause
+  exit /b 1c
+)
+
+IF "%3"=="" (
   echo "Missing QT path ie: C:\Qt\5.14.1\msvc2017"
   pause
   exit /b 1c
 )
 
-SET QTDIR=%1
-SET PROJDIR=%PWD%
+SET MSVC=%1
+SET ARCH=%2
+SET QTDIR=%3
+
 SET BUILDTOOL=jom
+SET vcarch=%ARCH%
+SET usbarch=%ARCH%
+IF "%ARCH%" == "x64" SET vcarch=amd64
+IF "%ARCH%" == "x86" SET usbarch=Win32
+IF "%MSVC%" == "2017" SET MSVC_VER=v141
+IF "%MSVC%" == "2019" SET MSVC_VER=v142
+SET PROJDIR=%CD%
+SET BUILDDIR=%PROJDIR%\..\build-qtusb-%ARCH%
 
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\%MSVC%\Community\VC\Auxiliary\Build\vcvarsall.bat" %vcarch%
 
@@ -18,14 +40,16 @@ SET PATH=%QTDIR%\bin;C:\Qt\Tools\QtCreator\bin;%PATH%
 WHERE /Q jom
 IF %errorlevel% NEQ 0 set BUILDTOOL=nmake
 
-git submodule update --init --recursive
+git submodule update --init --recursive.
 
-RMDIR /S /Q build
-MKDIR ../build-qtusb-msvc
-CD ../build-qtusb-msvc
-%QTDIR%\bin\qmake.exe %PROJDIR%
-%BUILDTOOL% install
+echo %BUILDDIR%
+
+RMDIR /S /Q %BUILDDIR%
+MKDIR %BUILDDIR%
+CD %BUILDDIR%
+%QTDIR%\bin\qmake.exe CONFIG+=static_lib %PROJDIR%
+%BUILDTOOL% sub-tests
 CD tests\auto
-%BUILDTOOL% check
+nmake check
 
 endlocal
