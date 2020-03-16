0ffe3219d9b09b8ea5a20a19b15110e76b7bac0e
Fabien Poussin <fabien.poussin@gmail.com>
2020-03-16 12:28:17 +0100

    appveyor

--- build_msvc.bat
+++ build_msvc.bat
@@ -25,6 +25,11 @@ IF "%4"=="" (
   exit /b 1c
 )
 
+SET INSTALLPATH=""
+IF NOT "%5"=="" (
+  SET INSTALLPATH=%5
+)
+
 SET MSVC=%1
 SET ARCH=%2
 SET LIBTYPE=%3
@@ -36,7 +41,7 @@ SET usbarch=%ARCH%
 IF "%ARCH%" == "x64" SET vcarch=amd64
 IF "%ARCH%" == "x86" SET usbarch=Win32
 SET PROJDIR=%CD%
-SET BUILDDIR=%PROJDIR%\..\build-qtusb-%ARCH%
+SET BUILDDIR=%PROJDIR%\build
 
 SET STATIC=""
 IF "%LIBTYPE%" == "static" SET STATIC="CONFIG+=staticlib"
@@ -56,9 +61,16 @@ RMDIR /S /Q %BUILDDIR%
 MKDIR %BUILDDIR%
 CD %BUILDDIR%
 %QTDIR%\bin\qmake.exe %STATIC% %PROJDIR%
-%BUILDTOOL%
-%BUILDTOOL% uninstall
-%BUILDTOOL% install
-%BUILDTOOL% docs install_docs
+
+IF NOT "%INSTALLPATH%"=="" (
+  RMDIR /S /Q %INSTALLPATH%
+  MKDIR %INSTALLPATH%
+  %BUILDTOOL% INSTALL_ROOT=%INSTALLPATH% install docs install_docs
+)
+ELSE (
+  %BUILDTOOL% install docs install_docs
+)
+%BUILDTOOL% sub-tests
+
 
 endlocal
